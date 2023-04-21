b2a849db9575b39ee068d7f5ae7aa8c942e5e96e
unknown <emilymora1207@gmail.com>
2023-04-21 16:09:29 -0700

    finalized routes

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # E-commerce-back-end
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+This project is used to help retail companies stay organized using back end Development. This will give retail companies an advantage to compete with other companies. I learned how to better navigate through the back end of development. 
+
+## Installation
+
+To install this project:
+1. open your integrated terminal. 
+2. use `npm i` to install all necessary dependencies. 
+3. add a .env file to use your mysql. 
+4. go to your mysql and use `mysql -u root -p` and type in your password. 
+5. create the ecommerce_db by typing `SOURCE db/schema.sql`.
+6. exit mysql.
+
+## Usage
+
+To use this project you can go to your browser or to insomnia. Navigate to the appropriate route to view your categories, products, or tags.  
+
+
+![screenshot of project](assets/images/screenshot.png)
+
+## Credits
+
+I used express to create the server in this cite. Mysql2 was used to create the database. and Sequalize was used to create models for the tables. 
+
+## License
+
+MIT
+
+https://github.com/Emilymora1207/E-commerce-back-end.git
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,11 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findAll({
+    const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
     });
     res.status(200).json(productData);
@@ -18,11 +18,11 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findByPk(req.params.id, {
+    const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Tag }],
     });
 
@@ -38,50 +38,38 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
-  try {
-    const productData = Product.create(req.body);
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  // Product.create(req.body)
-  //   .then((product) => {
-  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-  //     if (req.body.tagIds.length) {
-  //       const productTagIdArr = req.body.tagIds.map((tag_id) => {
-  //         return {
-  //           product_id: product.id,
-  //           tag_id,
-  //         };
-  //       });
-  //       return ProductTag.bulkCreate(productTagIdArr);
-  //     }
-  //     // if no product tags, just respond
-  //     res.status(200).json(product);
-  //   })
-  //   .then((productTagIds) => res.status(200).json(productTagIds))
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.status(400).json(err);
-  //   });
+router.post('/', async (req, res) => {
+
+  const productData = await Product.create(req.body)
+    .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
-  Product.update(req.body, 
+  const updatedProduct = await Product.update(req.body, 
     {
       // All the fields you can update and the data attached to the request body.
-      product_name: req.body.category_name,
+      product_name: req.body.product_name,
     },
     {
     where: {
@@ -122,10 +110,10 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = Product.destroy({
+    const productData = await Product.destroy({
       where: {
         id: req.params.id
       }
